b25dd48943123e775ae26c61980a5e77edf3ac1e
Rajat <rajat.bhargav.pc@gmail.com>
2026-03-21 01:31:27 +0530

    made constant keys for accessing loal storage

---
+++ src/constants/storageKeys.js
@@ -0,0 +1,9 @@
+/* 
+    This file contains the constant we are using to access local storage
+    DOCTORS -> stores all the doctors data
+    REMINDERS -> stores all the reminders data
+*/
+export const STORAGE_KEYS = {
+    DOCTORS: "carebridge_doctors",
+    REMINDERS: "carebridge_reminders"
+};
