b59daa1ac3a06fc97d8628bb67a99e9e683a900c
Prtyaksh-Dahiya <prtyakshdahiya@gmail.com>
2026-03-29 20:07:24 +0530

    Made changes in filter based on distance

--- src/utils/filterDoctors.js
+++ src/utils/filterDoctors.js
@@ -1,11 +1,27 @@
 import { isEqual } from "./validation";
+import { getDistance } from "./validation";
+import { geocodePlace } from "../services/geocodeService";
 export const filterDoctors = (doctors, { specialty = '',area='' } = {}) => {
   if (!specialty && !area) return doctors;
+  if(specialty && area){
+    const loc=geocodePlace(area);
   return doctors.filter((d) =>
   {
-    const specialtyMatch=isEqual(specialty,d.specialty);
-    const areaMatch=isEqual(area,d.area);
-    return specialtyMatch || areaMatch;
+      return isEqual(d.specialty,specialty);
   }
-  );
+  ).sort((doc1,doc2)=>{
+    return getDistance(doc1.lat,doc1.lng,loc.lat,loc.lng)-getDistance(doc2.lat,doc2.lng,loc.lat,loc.lng);
+  })
+}
+  if(specialty){
+    return doctors.filter((d)=> {
+      return isEqual(d.specialty,specialty);
+    })
+  }
+  if(area){
+    const loc=geocodePlace(area);
+    return doctors.sort((doc1,doc2)=>{
+      return getDistance(doc1.lat,doc1.lng,loc.lat,loc.lng)-getDistance(doc2.lat,doc2.lng,loc.lat,loc.lng);
+    })
+  }
 };
