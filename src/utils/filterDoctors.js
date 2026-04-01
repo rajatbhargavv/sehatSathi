129fc42c2a7ffcf7dcfe6acabbdf8b428c96da81
Rajat <rajat.bhargav.pc@gmail.com>
2026-04-01 19:33:54 +0530

    Merge mishthi changes into main by resolving filterDoctors.js conflict

--- src/utils/filterDoctors.js
+++ src/utils/filterDoctors.js
@@ -1,27 +1,16 @@
 import { isEqual } from "./validation";
-import { getDistance } from "./validation";
-import { geocodePlace } from "../services/geocodeService";
-export const filterDoctors = (doctors, { specialty = '',area='' } = {}) => {
+
+export const filterDoctors = (doctors, { specialty = "", area = "" } = {}) => {
   if (!specialty && !area) return doctors;
-  if(specialty && area){
-    const loc=geocodePlace(area);
-  return doctors.filter((d) =>
-  {
-      return isEqual(d.specialty,specialty);
-  }
-  ).sort((doc1,doc2)=>{
-    return getDistance(doc1.lat,doc1.lng,loc.lat,loc.lng)-getDistance(doc2.lat,doc2.lng,loc.lat,loc.lng);
-  })
-}
-  if(specialty){
-    return doctors.filter((d)=> {
-      return isEqual(d.specialty,specialty);
-    })
-  }
-  if(area){
-    const loc=geocodePlace(area);
-    return doctors.sort((doc1,doc2)=>{
-      return getDistance(doc1.lat,doc1.lng,loc.lat,loc.lng)-getDistance(doc2.lat,doc2.lng,loc.lat,loc.lng);
-    })
-  }
+
+  return doctors.filter((d) => {
+    const specialtyMatch = specialty ? isEqual(specialty, d.specialty) : true;
+    const areaMatch = area ? isEqual(area, d.area) : true;
+
+    if (specialty && area) {
+      return specialtyMatch && areaMatch;
+    }
+
+    return specialtyMatch || areaMatch;
+  });
 };
